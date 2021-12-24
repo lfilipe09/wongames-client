a18ba703404a1093bf691529687604a6e832b1ac
lfilipe09 <81202572+lfilipe09@users.noreply.github.com>
2021-12-24 10:32:50 -0300

    Adding Plop file creation

---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Button from '.'
+
+export default {
+  title: 'Main',
+  component: Button
+} as Meta
+
+export const Basic: Story = () => <Button />
